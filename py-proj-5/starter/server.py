106763788b70892ac960a1264d4cde9d35c8bd22
Damien Redd <damiens.redd@gmail.com>
2023-04-18 16:13:38 -0500

    basic working functions

--- py-proj-5/starter/server.py
+++ py-proj-5/starter/server.py
@@ -16,6 +16,27 @@ app.jinja_env.undefined = StrictUndefined
 def homepage():
     return render_template("homepage.html")
 
+@app.route("/movies") #route to view all movies page
+def all_movies():
+    movies = crud.get_movies() #gets movies and assigns them to a variable
+    return render_template("all_movies.html", movies=movies)
+
+@app.route("/movies/<movie_id>") #route to display a movies info
+def show_movie(movie_id): #func to get movie details
+    movie = crud.get_movie_by_id(movie_id)
+    return render_template("movie_details.html", movie=movie)
+
+@app.route("/users")
+def all_users():
+    users = crud.get_users()
+    return render_template("/all_users.html", users=users)
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    user = crud.get_user_by_id(user_id)
+    return render_template("user_details.html", user=user)
+
+
 
 
 
